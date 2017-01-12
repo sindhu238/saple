fefe6bb732c2fe80629449765d146c99d91c8755
sindhu238 <saridesindhu@gmail.com>
2017-01-12 12:25:54 +0000

    Add files via upload

---
+++ Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Skylite Travels
+//
+//  Created by Venkateswara on 05/01/17.
+//  Copyright © 2017 Sindhu. All rights reserved.
+//
+
+import Foundation
+
+
+let API_KEY = "AIzaSyDgiWy_BU4-Oyj8C1JIzCozTDqauywjXns"
+let base_url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+
+typealias DownloadComplete = () -> ()
+
+
+    
